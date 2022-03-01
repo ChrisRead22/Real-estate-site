20878fbd2790e9c5c702d34199ee25010b2efcef
Christopher Read <Christopherread22@outlook.com>
2022-02-28 19:35:26 -0500

    components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+height: 60px;
+background: red;
+`
 
 const Navbar = () => {
   return (
     <div>
         <h1>Navbar</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
