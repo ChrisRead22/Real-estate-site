b342b20fe0c5b5f0cad319dd44aae426cd82ed22
Christopher Read <Christopherread22@outlook.com>
2022-03-03 17:36:58 -0500

    components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 
 const Nav = styled.nav`
 height: 60px;
 background: red;
+`;
+
+const Logo = styled(Link)`
+color: #fff;
 `
+
 
 const Navbar = () => {
   return (
     <Nav>
       <Logo>ELIXR</Logo>
+      <MenuBars />
+      <NavMenu>
+        
+      </NavMenu>
         <h1>Navbar</h1>
     </Nav>
   );
