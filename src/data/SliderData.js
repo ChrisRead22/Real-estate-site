77d82f0ad0f342731a26e425fc6a7819f21956cf
Christopher Read <Christopherread22@outlook.com>
2022-03-18 22:07:07 -0400

    New data

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -7,5 +7,28 @@ exports const SliderData = [
         image: ImageOne,
         alt: 'House'
     },
-    
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: '$2,860,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+    {
+        title: 'House in Calabasas, California',
+        price: '$8,420,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+    {
+        title: 'House in Houston, Texas',
+        price: '$6,660,000',
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    }
 ]
