963b85a396160d2fa6e48a625785a9efc778640d
Christopher Read <Christopherread22@outlook.com>
2022-03-28 19:23:51 -0400

    Content

--- src/components/Complex.js
+++ src/components/Complex.js
@@ -82,7 +82,17 @@ width: calc(100% - 100px);
 color: #fff;
 
 h1 {
-  
+  font-size: clamp(1rem, 8vw, 20rem);
+  font-weight: 400;
+  text-transform: uppercase;
+  text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  text-align: left;
+  margin-bottom: 0.8rem;
+}
+
+p {
+  margin-bottom: 1.2rem;
+  text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
 }
 `;
 
