27c85a5c3d394ee0588c4d616d751f0f7bc0bd12
Christopher Read <Christopherread22@outlook.com>
2022-03-31 19:59:16 -0400

    useEffect

--- src/components/Complex.js
+++ src/components/Complex.js
@@ -133,8 +133,20 @@ const length = slides.length
 const timeout = useRef(null)
 
 useEffect(() => {
+  const nextSlide = () => {
+    setCurrent(current => (current === length - 1 ? 0 : current + 1))
+  }
 
-}, [current, length])
+  timeout.current = setTimeout(nextSlide, 3000)
+
+  return function () {
+    if(timeout.current) {
+      clearTimeout(timeout.current);
+    }
+  };
+}, 
+  [current, length]
+);
 
 const nextSlide = () => {
   setCurrent(current === length - 1 ? 0 : current + 1);
