945adddccf1eeb5c3dedaa6b153615d0958cf163
Christopher Read <Christopherread22@outlook.com>
2022-04-07 16:42:06 -0400

    new component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 const DropdownContainer = styled.div`
+position: fixed;
+z-index: 999;
+width: 100%;
+height: 100;
+background: #cd853f;
+display: grid;
+align-items: center;
+top: 0;
 
 `
 
