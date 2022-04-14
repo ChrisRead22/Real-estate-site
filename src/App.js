ad0575d18093ef7560a00497d580e8a1b57f7805
Christopher Read <Christopherread22@outlook.com>
2022-04-14 19:20:47 -0400

    opacity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Complex from './components/Complex';
 import Navbar from './components/Navbar';
+import Dropdown from './components/Dropdown';
 import { SliderData } from './data/SliderData';
 import GlobalStyle from './globalStyles';
 
