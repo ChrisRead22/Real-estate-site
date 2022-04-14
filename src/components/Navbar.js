a0f3c11f5fe0fb51afd65ed4a99d1a032e0c1baf
Christopher Read <Christopherread22@outlook.com>
2022-04-13 20:50:04 -0400

    toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,11 +63,11 @@ margin-right: 24px;
 `;
 
 
-const Navbar = () => {
+const Navbar = ({toggle}) => {
   return (
     <Nav>
       <Logo to="/">ELIXR</Logo>
-      <MenuBars />
+      <MenuBars onClick={toggle}/>
       <NavMenu>
         {menuData.map((item, index) => (
           <NavMenuLinks to={item.link} key={index}>
